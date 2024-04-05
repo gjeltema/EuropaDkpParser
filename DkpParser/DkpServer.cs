cfd4851744a28f60bba5ba9e7d80f2e7e1afa222
Craig Gjeltema <gjeltemac@outlook.com>
2024-04-06 00:12:43 +0200

    Added update-on-character to a couple of text boxes.  Also stubbed out the uploader a bit more.

---
+++ DkpParser/DkpServer.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// DkpServer.cs Copyright 2024 Craig Gjeltema
+// -----------------------------------------------------------------------
+
+namespace DkpParser;
+
+public sealed class DkpServer : IDkpServer
+{
+    private readonly DkpParserSettings _settings;
+
+    public DkpServer(DkpParserSettings settings)
+    {
+        _settings = settings;
+    }
+
+    public DkpServerMessageResult UploadAttendance(AttendanceEntry attendanceEntry)
+    {
+        DkpServerMessageResult result = new();
+
+        return result;
+    }
+
+    public DkpServerMessageResult UploadDkpSpent(DkpEntry attendanceEntry)
+    {
+        DkpServerMessageResult result = new();
+
+        return result;
+    }
+}
+
+public sealed class DkpServerMessageResult
+{
+
+}
+
+public interface IDkpServer
+{
+    DkpServerMessageResult UploadAttendance(AttendanceEntry attendanceEntry);
+
+    DkpServerMessageResult UploadDkpSpent(DkpEntry dkpEntry);
+}
--- DkpParser/RaidUploader.cs
+++ DkpParser/RaidUploader.cs
@@ -1,28 +1,67 @@
 ﻿// -----------------------------------------------------------------------
-// FileOutputGenerator.cs Copyright 2024 Craig Gjeltema
+// RaidUploader.cs Copyright 2024 Craig Gjeltema
 // -----------------------------------------------------------------------
 
 namespace DkpParser;
 
 public sealed class RaidUploader : IRaidUpload
 {
+    private readonly IDkpServer _dkpServer;
     private readonly DkpParserSettings _settings;
 
     public RaidUploader(DkpParserSettings settings)
     {
         _settings = settings;
+        _dkpServer = new DkpServer(settings);
     }
 
     public RaidUploadResults UploadRaid(RaidEntries raidEntries)
     {
+        RaidUploadResults results = new();
 
-        return null;
+        UploadAttendances(raidEntries.AttendanceEntries, results);
+        UploadDkpSpendings(raidEntries.DkpEntries, results);
+
+        return results;
+    }
+
+    private void UploadAttendances(ICollection<AttendanceEntry> attendanceEntries, RaidUploadResults results)
+    {
+        foreach (AttendanceEntry attendance in attendanceEntries)
+        {
+            if (attendance.Players.Count > 1)
+            {
+                DkpServerMessageResult result = _dkpServer.UploadAttendance(attendance);
+                results.AddAttendanceResult(result);
+            }
+        }
+    }
+
+    private void UploadDkpSpendings(ICollection<DkpEntry> dkpEntries, RaidUploadResults results)
+    {
+        foreach (DkpEntry dkpEntry in dkpEntries)
+        {
+            DkpServerMessageResult result = _dkpServer.UploadDkpSpent(dkpEntry);
+            results.AddDkpSpentResult(result);
+        }
     }
 }
 
 public sealed class RaidUploadResults
 {
+    public ICollection<DkpServerMessageResult> AttendanceUploadResults { get; } = [];
 
+    public ICollection<DkpServerMessageResult> DkpSpentUploadResults { get; } = [];
+
+    public void AddAttendanceResult(DkpServerMessageResult result)
+    {
+
+    }
+
+    public void AddDkpSpentResult(DkpServerMessageResult result)
+    {
+
+    }
 }
 
 public interface IRaidUpload
