466e7bdeb41c43e332dcc03d95f50344a6711edb
Craig Gjeltema <gjeltemac@outlook.com>
2024-10-26 13:57:35 +0200

    LiveLogTracking screen interim commit.

---
+++ EuropaDkpParser/ViewModels/LiveLogTrackingViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace EuropaDkpParser.ViewModels;
+
+using DkpParser;
+using EuropaDkpParser.Resources;
+using Prism.Commands;
+
+internal sealed class LiveLogTrackingViewModel : DialogViewModelBase, ILiveLogTrackingViewModel
+{
+    public LiveLogTrackingViewModel(IDialogViewFactory viewFactory) 
+        : base(viewFactory)
+    {
+        Title = Strings.GetString("LiveLogTrackingDialogTitleText");
+
+
+    }
+}
+
+public interface ILiveLogTrackingViewModel : IDialogViewModel
+{
+
+}
